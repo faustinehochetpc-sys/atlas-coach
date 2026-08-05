3f6077b716c994587aa151244845f1fd063116d7
faustinehochetpc-sys <faustinehochetpc@gmail.com>
2026-08-05 15:32:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ if user_input:
         else:
             try:
                 genai.configure(api_key=api_key)
-                model = genai.GenerativeModel("gemini-1.5-flash")
+                model = genai.GenerativeModel("gemini-1.5-flash-latest")
                 
                 # Extraction des documents joints si présents
                 context_fichiers = ""
